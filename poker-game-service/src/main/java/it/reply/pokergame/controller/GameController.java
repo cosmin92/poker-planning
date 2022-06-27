d546fa5e5cc8ff52e9693bec65332bda4da9431a
Gregorio Brembilla <gbrembilla@red.software.systems>
2022-06-27 15:39:00 +0200

    features) add endpoint logic for GameController

--- poker-game-service/src/main/java/it/reply/pokergame/controller/GameController.java
+++ poker-game-service/src/main/java/it/reply/pokergame/controller/GameController.java
@@ -7,16 +7,18 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.reply.pokergame.model.ErrorResponseModel;
+import it.reply.pokergame.model.GameDto;
 import it.reply.pokergame.model.Player;
 import it.reply.pokergame.service.GameService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Validated
 @RestController
@@ -27,6 +29,34 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Get information of game",
+            description = "Get Game",
+            tags = { "game Controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Ok",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Integer.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content =  @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponseModel.class))),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content())
+            }
+    )
+    public ResponseEntity<String> gameCreation(
+            @RequestParam(value = "gameName", required = true) String gameName,
+            @RequestParam(value = "admin", required = true) Long playerId,
+            @RequestParam(value = "link", required = true) String link
+    ) {
+        log.info("Entered endpoint: POST '/game/gameCreation'");
+        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{gameId}")
+                .buildAndExpand(gameService.gameCreation(gameName, playerId, link)).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
 
     @Operation(
             summary = "Get information of game",
@@ -42,13 +72,9 @@ public class GameController {
                     @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content())
             }
     )
-    @GetMapping("/gameCreation")
-    public ResponseEntity<String> gameCreation(
-            @RequestParam(value = "gameName", required = true) String gameName,
-            @RequestParam(value = "admin", required = true) Player admin,
-            @RequestParam(value = "link", required = true) String link
-    ) {
-        log.info("Entered endpoint: GET '/game/gameCreation'");
-        return ResponseEntity.ok(gameService.gameCreation(gameName, admin, link));
+    @GetMapping("/findGame")
+    public ResponseEntity<GameDto> findGame(@RequestParam(value = "idGame", required = true) Long idGame) {
+        log.info("Entered endpoint: POST '/game/findGame'");
+        return ResponseEntity.ok(gameService.findGame(idGame));
     }
 }
