db505b974a88b9f2116fd67d32249d5cd752f99d
monica-am <m.ammirata@reply.it>
2022-07-01 11:16:15 +0200

    fix login inputs

--- fe-poker-planning/src/components/Login.tsx
+++ fe-poker-planning/src/components/Login.tsx
@@ -41,39 +41,43 @@ const Login: React.FC = () => {
         title="Login"
         onOk={handleOk}
         onCancel={handleCancel}
-        footer={[
-          <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
-            Submit
-          </Button>,
-          <p>Create Account</p>
-        ]}
       >
 
         <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+              name="basic"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
             >
-            <Form.Item
+              <Form.Item
                 label="Username"
                 name="username"
                 rules={[{ required: true, message: 'Please input your username!' }]}
-            >
+              >
                 <Input />
-            </Form.Item>
+              </Form.Item>
 
-            <Form.Item
+              <Form.Item
                 label="Password"
                 name="password"
                 rules={[{ required: true, message: 'Please input your password!' }]}
-            >
+              >
                 <Input.Password />
-            </Form.Item>
-        </Form>
+              </Form.Item>
+
+              <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit" onClick={() => handleOk()}>
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
       </Modal>
     </>
   );
