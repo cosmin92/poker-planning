e2791bf8eaaa9beac950fb4d0d2aa56a24be70f7
Adrian Ungureanu <a.ungureanu@reply.it>
2022-07-04 16:29:41 +0200

    Fix merge errors

--- poker-planning-online-v2/src/service/Players.ts
+++ poker-planning-online-v2/src/service/Players.ts
@@ -73,7 +73,6 @@ export const resetGame = async (gameId: string) => {
             value: 0,
         };
 
-<<<<<<< HEAD
         await updatePlayerInStore(gameId, updatedPlayer);
     })
 
@@ -89,12 +88,5 @@ export const resetGame = async (gameId: string) => {
     //     return true;
     // }
     // return false;
-=======
-        updatePlayerInStore(gameId, ...players);
-        await updateGameStatus(gameId);
-        return true;
-    }
-    return false;
->>>>>>> 0a5d76ecb7c0335cc350bae4bc22d57ea417dce0
 
 };
